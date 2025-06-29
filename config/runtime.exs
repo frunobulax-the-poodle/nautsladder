4c6ef50f2b20d8af81fbe0a06dd1977c7901a0ed
frunobulax <frunobulax@tuta.io>
2025-06-29 17:45:44 +0200

    Don't require discord credentials in tests

--- config/runtime.exs
+++ config/runtime.exs
@@ -116,13 +116,15 @@ if config_env() == :prod do
   # See https://hexdocs.pm/swoosh/Swoosh.html#module-installation for details.
 end
 
-discord_id = System.get_env("DISCORD_CLIENT_ID") || raise "Required DISCORD_CLIENT_ID in env."
+if config_env() != :test do
+  discord_id = System.get_env("DISCORD_CLIENT_ID") || raise "Required DISCORD_CLIENT_ID in env."
 
-discord_secret =
-  System.get_env("DISCORD_CLIENT_SECRET") || raise "Required DISCORD_CLIENT_ID in env."
+  discord_secret =
+    System.get_env("DISCORD_CLIENT_SECRET") || raise "Required DISCORD_CLIENT_ID in env."
 
-config :assent,
-  discord: [
-    client_id: discord_id,
-    client_secret: discord_secret
-  ]
+  config :assent,
+    discord: [
+      client_id: discord_id,
+      client_secret: discord_secret
+    ]
+end
